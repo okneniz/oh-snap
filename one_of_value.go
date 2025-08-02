47471409c896c2cecd346a4874ff32fd94bc6077
Andrey Zinenko <zinenkoan@yahoo.com>
2025-08-02 13:28:03 +0300

    add arbitrary value from slice

---
+++ one_of_value.go
@@ -0,0 +1,29 @@
+package ohsnap
+
+import (
+	"math/rand/v2"
+)
+
+type arbitraryOneOfValue[T any] struct {
+	rand   *rand.Rand
+	values []T
+}
+
+func OneOValue[T any](
+	rnd *rand.Rand,
+	values []T,
+) Arbitrary[T] {
+	return &arbitraryOneOfValue[T]{
+		rand:   rnd,
+		values: values,
+	}
+}
+
+func (a *arbitraryOneOfValue[T]) Generate() T {
+	idx := a.rand.IntN(len(a.values))
+	return a.values[idx]
+}
+
+func (a *arbitraryOneOfValue[T]) Shrink(value T) []T {
+	return nil
+}
